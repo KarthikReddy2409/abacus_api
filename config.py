b05a0378f8b8890632332386a7fa7015da3b6760
Karthik <karthik@Mac.lan>
2026-01-22 10:12:20 -0500

    Adding Redis configuration

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+REDIS_DB = int(os.getenv("REDIS_DB", 0))
+
+SUM_KEY = "abacus:sum"  # redis key for the running sum
